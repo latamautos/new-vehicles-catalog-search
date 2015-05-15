7731543b14d0b88dc94e12920f66152cdc62b333
Paul Jarrin <paul.jarrin@latamautos.com>
2015-05-15 15:45:15 -0500

    Se agrega cliente de tag

--- src/Latamautos/NewVehiclesCatalogSearch/NewVehiclesCatalogSearchServiceProvider.php
+++ src/Latamautos/NewVehiclesCatalogSearch/NewVehiclesCatalogSearchServiceProvider.php
@@ -27,6 +27,7 @@ class NewVehiclesCatalogSearchServiceProvider extends ServiceProvider {
 		$this->app->bind('Latamautos\NewVehiclesCatalogSearch\application\contract\IBrandService', 'Latamautos\NewVehiclesCatalogSearch\application\object\BrandService');
 		$this->app->bind('Latamautos\NewVehiclesCatalogSearch\application\contract\ISubtypeService', 'Latamautos\NewVehiclesCatalogSearch\application\object\SubtypeService');
 		$this->app->bind('Latamautos\NewVehiclesCatalogSearch\application\contract\IModelService', 'Latamautos\NewVehiclesCatalogSearch\application\object\ModelService');
+		$this->app->bind('Latamautos\NewVehiclesCatalogSearch\application\contract\ITagService', 'Latamautos\NewVehiclesCatalogSearch\application\object\TagService');
 	}
 
 	/**
