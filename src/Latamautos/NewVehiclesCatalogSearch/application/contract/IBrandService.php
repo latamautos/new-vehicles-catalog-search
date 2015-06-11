443a997093d093b538e996becbf2f7690936c943
Paul Jarrin <paul.jarrin@latamautos.com>
2015-06-11 17:42:23 -0500

    Servicio para subir imagenes en brand

--- src/Latamautos/NewVehiclesCatalogSearch/application/contract/IBrandService.php
+++ src/Latamautos/NewVehiclesCatalogSearch/application/contract/IBrandService.php
@@ -13,4 +13,5 @@ use Latamautos\MicroserviceGateway\core\IResource;
 
 interface IBrandService extends IResource {
 
+	public function addOrUpdateImage($image);
 }
--- src/Latamautos/NewVehiclesCatalogSearch/application/object/BrandService.php
+++ src/Latamautos/NewVehiclesCatalogSearch/application/object/BrandService.php
@@ -8,14 +8,37 @@
 
 namespace Latamautos\NewVehiclesCatalogSearch\application\object;
 
+use grupocentrico\basecode\helpers\Thumbnail;
+use latam\core\domain\contract\IImageHandler;
 use Latamautos\NewVehiclesCatalogSearch\application\contract\IBrandService;
 use Latamautos\NewVehiclesCatalogSearch\application\dto\CatalogDTO;
 
 class BrandService extends BaseNewVehicleCatalogService implements IBrandService {
 
-	function __construct() {
+	function __construct(IImageHandler $imageHandler) {
 		parent::__construct();
 		$this->setDto(new CatalogDTO());
 		$this->setUri("new-vehicles-catalog/v1/catalogs/brands");
+		$this->imageHandler = $imageHandler;
+	}
+
+	public function addOrUpdateImage($image) {
+		$image = $this->imageHandler->createCatalogNewsImage('brands', $image);
+		return $this->buildPathOfPhoto($image->getResourceId(), $image->getName(),$image->getBucketHash());
+	}
+
+	/**
+	 * @param $resourceId
+	 * @param $namePhoto
+	 * @param $imageBucketHash
+	 * @return string
+	 * @internal param $image
+	 */
+	private function buildPathOfPhoto($resourceId, $namePhoto, $imageBucketHash) {
+		if ($namePhoto == null) {
+			return null;
+		}
+		$imageComplete = "catalogNews/" . $resourceId . "/" . $namePhoto;
+		return Thumbnail::getFromBucket($imageComplete, $imageBucketHash,$args = '300x198');
 	}
 }
